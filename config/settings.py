d857324f10ba09142cb5eda7a6fa708fb2d4587e
김민우 <128016486+power-minu@users.noreply.github.com>
2024-07-18 17:37:07 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'config.urls'
 
